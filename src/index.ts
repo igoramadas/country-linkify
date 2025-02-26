266000fd8364caa05e53a200be507588f5d6ee40
Igor Ramadas <igor@devv.com>
2025-02-26 21:30:20 +0100

    Support passing settings directly to the startup() routine.

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,24 @@ import linkManager from "./linkmanager"
 import server from "./server"
 import express = require("express")
 
-// Startup script.
-const startup = async (app?: express.Express) => {
+/**
+ * Startup routine to load settings and init the web server.
+ * @param settings Optional settings object to be loaded (on top of the defaults).
+ * @param app Optional web server where the routes should be attached to.
+ */
+const startup = async (settings?: any, app?: express.Express) => {
     logger.info("CountryLinkify.startup", `PID ${process.pid}`)
 
     try {
         setmeup.load(__dirname + "/../settings.default.json", {overwrite: false})
 
+        // Load default and custom settings.
         if (!setmeup.settings.countryLinkify) {
             setmeup.loadFromEnv()
             setmeup.load(__dirname + "/../settings.local.json")
+        }
+        if (settings) {
+            setmeup.loadJson({countryLinkify: settings.countryLinkify || settings})
         }
 
         // Start everything.
